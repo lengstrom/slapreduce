71dea8a05e79062bfef7a7e5ab11b09936c64143
Logan Engstrom <engstrom@mit.edu>
2024-11-27 18:43:47 -0500

    add setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name="slapreduce",
+      version="0.1",
+      description="slapreduce",
+      packages=['slapreduce'],
+      include_package_data=True,
+      )
